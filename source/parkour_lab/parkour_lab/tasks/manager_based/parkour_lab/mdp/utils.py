45fbb992ad0e600b963276396be9d2f928c89770
Leon Bai <leon.bai@tum.de>
2026-06-20 23:25:38 +0200

    Enhance docstrings for _robot_root_pos_env and _goal_distance functions

--- source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/utils.py
+++ source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/utils.py
@@ -6,11 +6,25 @@ import torch
 
 
 def _robot_root_pos_env(env: ManagerBasedRLEnv, asset_cfg=SceneEntityCfg("robot")) -> torch.Tensor:
+    """
+    Robot root position in each environment's local frame.
+
+    Returns:
+        [num_envs, 3]
+    """
+
     asset: Articulation = env.scene[asset_cfg.name]
     return asset.data.root_pos_w - env.scene.env_origins
 
 
 def _goal_distance(env: ManagerBasedRLEnv, goal_cfg=SceneEntityCfg("goal"), asset_cfg=SceneEntityCfg("robot")) -> torch.Tensor:
+    """
+    XYZ distance from robot root to goal.
+
+    Returns:
+        [num_envs]
+    """
+
     robot_root_pos = _robot_root_pos_env(env, asset_cfg)
     goal: AssetBase = env.scene[goal_cfg.name]
     goal_pos = goal.data.root_pos_w - env.scene.env_origins
