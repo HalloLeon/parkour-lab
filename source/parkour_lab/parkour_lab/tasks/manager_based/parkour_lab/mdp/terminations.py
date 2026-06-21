ee87f8379874f382ff26cf11881280d324e4a0cc
Leon Bai <leon.bai@tum.de>
2026-06-21 22:20:41 +0200

    Refactor reached_goal function to include base height check and rename to reached_goal_xy

--- source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/terminations.py
+++ source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/terminations.py
@@ -5,6 +5,25 @@ import torch
 from . import utils
 
 
-def reached_goal(env: ManagerBasedRLEnv, threshold: float, goal_cfg=SceneEntityCfg("goal"), asset_cfg=SceneEntityCfg("robot")) -> torch.Tensor:
-    dist_to_goal = utils._goal_distance_xyz(env, goal_cfg, asset_cfg)
-    return (dist_to_goal < threshold)
+def reached_goal_xy(
+    env: ManagerBasedRLEnv,
+    threshold: float,
+    min_base_height: float = 0.22,
+    goal_cfg=SceneEntityCfg("goal"),
+    asset_cfg=SceneEntityCfg("robot"),
+) -> torch.Tensor:
+    """
+    Success termination based on XY distance to goal,
+    while requiring the robot not to be collapsed.
+
+    Returns:
+        [num_envs]
+    """
+
+    dist_to_goal = utils._goal_distance_xy(env, goal_cfg, asset_cfg)
+    base_height = utils._robot_base_height(env, asset_cfg)
+
+    reached = dist_to_goal < threshold
+    base_high_enough = base_height > min_base_height
+
+    return torch.logical_and(reached, base_high_enough)
