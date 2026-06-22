f1919b75a9e685b0b1a0eccfd99c43f466dcbc35
Leon Bai <leon.bai@tum.de>
2026-06-22 22:27:37 +0200

    Swap implementations of goal_distance_xy_w and base_height_w functions for clarity and consistency in observation calculations

--- source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/observations.py
+++ source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/observations.py
@@ -5,24 +5,9 @@ import torch
 from . import utils
 
 
-def goal_distance_xy(
-    env: ManagerBasedRLEnv,
-    goal_cfg=SceneEntityCfg("goal"),
-    asset_cfg=SceneEntityCfg("robot"),
-) -> torch.Tensor:
-    """
-    XY distance from robot root to goal.
-
-    Returns:
-        [num_envs, 1]
-    """
-
-    return utils._goal_distance_xy(env, goal_cfg, asset_cfg).unsqueeze(-1)
-
-
 def base_height_w(
     env: ManagerBasedRLEnv,
-    asset_cfg=SceneEntityCfg("robot"),
+    asset_cfg=SceneEntityCfg("robot")
 ) -> torch.Tensor:
     """
     Robot base/root height.
@@ -34,7 +19,26 @@ def base_height_w(
     return utils._robot_base_height(env, asset_cfg).unsqueeze(-1)
 
 
-def goal_distance_xyz_w(env: ManagerBasedRLEnv, goal_cfg=SceneEntityCfg("goal"), asset_cfg=SceneEntityCfg("robot")) -> torch.Tensor:
+def goal_distance_xy_w(
+    env: ManagerBasedRLEnv,
+    goal_cfg=SceneEntityCfg("goal"),
+    asset_cfg=SceneEntityCfg("robot")
+) -> torch.Tensor:
+    """
+    XY distance from robot root to goal.
+
+    Returns:
+        [num_envs, 1]
+    """
+
+    return utils._goal_distance_xy(env, goal_cfg, asset_cfg).unsqueeze(-1)
+
+
+def goal_distance_xyz_w(
+    env: ManagerBasedRLEnv,
+    goal_cfg=SceneEntityCfg("goal"),
+    asset_cfg=SceneEntityCfg("robot")
+) -> torch.Tensor:
     """
     XYZ distance from robot root to goal.
 
@@ -43,3 +47,22 @@ def goal_distance_xyz_w(env: ManagerBasedRLEnv, goal_cfg=SceneEntityCfg("goal"),
     """
 
     return utils._goal_distance_xyz(env, goal_cfg, asset_cfg).unsqueeze(-1)
+
+
+def goal_direction_xy_w(
+    env: ManagerBasedRLEnv,
+    goal_cfg=SceneEntityCfg("goal"),
+    asset_cfg=SceneEntityCfg("robot")
+) -> torch.Tensor:
+    """
+    Normalized XY direction from robot root to goal.
+
+    Returns:
+        [num_envs, 2]
+    """
+
+    to_goal_xy = utils._goal_vector_xy(env, goal_cfg, asset_cfg)
+
+    norm = torch.linalg.norm(to_goal_xy, dim=-1, keepdim=True).clamp_min(1.0e-6)
+
+    return to_goal_xy / norm
