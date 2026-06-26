0d96df96f24f2afcfcbc0704db29d54f28fe8c0d
Leon Bai <leon.bai@tum.de>
2026-06-26 11:59:37 +0200

    Remove minimum base height check from reached_goal_xy function

--- source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/terminations.py
+++ source/parkour_lab/parkour_lab/tasks/manager_based/parkour_lab/mdp/terminations.py
@@ -8,7 +8,6 @@ from . import utils
 def reached_goal_xy(
     env: ManagerBasedRLEnv,
     threshold: float,
-    min_base_height: float = 0.22,
     goal_cfg=SceneEntityCfg("goal"),
     asset_cfg=SceneEntityCfg("robot")
 ) -> torch.Tensor:
@@ -21,9 +20,6 @@ def reached_goal_xy(
     """
 
     dist_to_goal = utils._goal_distance_xy(env, goal_cfg, asset_cfg)
-    base_height = utils._robot_base_height(env, asset_cfg)
-
     reached = dist_to_goal < threshold
-    base_high_enough = base_height > min_base_height
 
-    return torch.logical_and(reached, base_high_enough)
+    return reached
